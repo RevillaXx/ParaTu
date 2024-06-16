b758eb2b1b3eace7a2fb816105acecdc274351c9
RevillaXx <victor.revilla2020@gmail.com>
2024-06-16 12:21:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Array con el listado de canciones a mostrar en el reprodutor
 const canciones = [
 "Aitana-Vas_A_Quedarte.mp3",
-"Anitta_&_J_Balvin_Downtown.mp3",
+"Anitta-&-J-Balvin-Downtown.mp3",
 "Aventura-LaBoda.mp3",
 "BensonBoone-BeautifulThings.mp3",
 "Camila - Solo Para Ti.mp3",
